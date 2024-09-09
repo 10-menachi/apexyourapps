14b9d4d81d605e9312eb20f4dd8949d8c00cb5d2
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-09-09 10:51:31 +0300

    Complete Features Section Design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,34 +27,7 @@
     @include('components.header')
     @include('components.navbar')
     @include('components.banner')
-
-    <!-- features -->
-    <div class="container py-16">
-        <div class="grid justify-center w-10/12 grid-cols-1 gap-6 mx-auto md:grid-cols-3">
-            <div class="flex items-center justify-center gap-5 px-3 py-6 border rounded-sm border-primary">
-                <img src="assets/images/icons/delivery-van.svg" alt="Delivery" class="object-contain w-12 h-12">
-                <div>
-                    <h4 class="text-lg font-medium capitalize">Free Shipping</h4>
-                    <p class="text-sm text-gray-500">Order over $200</p>
-                </div>
-            </div>
-            <div class="flex items-center justify-center gap-5 px-3 py-6 border rounded-sm border-primary">
-                <img src="assets/images/icons/money-back.svg" alt="Delivery" class="object-contain w-12 h-12">
-                <div>
-                    <h4 class="text-lg font-medium capitalize">Money Rturns</h4>
-                    <p class="text-sm text-gray-500">30 days money returs</p>
-                </div>
-            </div>
-            <div class="flex items-center justify-center gap-5 px-3 py-6 border rounded-sm border-primary">
-                <img src="assets/images/icons/service-hours.svg" alt="Delivery" class="object-contain w-12 h-12">
-                <div>
-                    <h4 class="text-lg font-medium capitalize">24/7 Support</h4>
-                    <p class="text-sm text-gray-500">Customer support</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ./features -->
+    @include('components.features')
 
     <!-- categories -->
     <div class="container py-16">
