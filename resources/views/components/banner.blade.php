c7f5a9be581234faae72c0258aaa7a3cd2dd5fe6
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-09-09 10:48:08 +0300

    Finish Styling Banner

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,16 @@
+<div class="bg-center bg-no-repeat bg-cover py-36" style="background-image: url('{{ url('banner.jpg') }}');">
+    <div class="container">
+        <h1 class="mb-4 text-6xl font-medium text-gray-800 capitalize">
+            best collection for <br> home decoration
+        </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam <br>
+            accusantium perspiciatis, sapiente
+            magni eos dolorum ex quos dolores odio</p>
+        <div class="mt-12">
+            <a href="#"
+                class="px-8 py-3 font-medium text-white border rounded-md bg-primary border-primary hover:bg-transparent hover:text-primary">Shop
+                Now</a>
+        </div>
+    </div>
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,24 +26,7 @@
 
     @include('components.header')
     @include('components.navbar')
-
-    <!-- banner -->
-    <div class="bg-center bg-no-repeat bg-cover py-36" style="background-image: url('assets/images/banner-bg.jpg');">
-        <div class="container">
-            <h1 class="mb-4 text-6xl font-medium text-gray-800 capitalize">
-                best collection for <br> home decoration
-            </h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam <br>
-                accusantium perspiciatis, sapiente
-                magni eos dolorum ex quos dolores odio</p>
-            <div class="mt-12">
-                <a href="#"
-                    class="px-8 py-3 font-medium text-white border rounded-md bg-primary border-primary hover:bg-transparent hover:text-primary">Shop
-                    Now</a>
-            </div>
-        </div>
-    </div>
-    <!-- ./banner -->
+    @include('components.banner')
 
     <!-- features -->
     <div class="container py-16">
