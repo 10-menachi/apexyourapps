395b91f948a00556c7c6587cfab65a4f7862e0b8
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-09-30 11:00:45 +0300

    "Refactored ProductController and routes/web.php to separate customer/guest and super admin functionality"

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,52 @@ Route::get('contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::resource('product', ProductController::class);
+// Route::resource('product', ProductController::class);
+
+//Product Controller Routes 
+/*************************************************************
+ * 
+ * 
+ * Customer  and guests user of the system
+ * 
+ * 
+ */
+
+Route::get('product', [ProductController::class, 'index'])->name('product.index');
+
+// Display the specified product.
+Route::get('product/{product}', [ProductController::class, 'show'])->name('product.show');
+
+/*************************************************************
+ * 
+ * 
+ * Super Admin of the system
+ * 
+ * 
+ * 
+ */
+Route::get('admin/product', [ProductController::class, 'adminIndex'])->name('admin.product.store');
+
+// Show the form for creating a new product.
+Route::get('admin/product/create', [ProductController::class, 'create'])->name('admin.product.store');
+
+// Store a newly created product in storage.
+Route::post('admin/product', [ProductController::class, 'store'])->name('admin.product.store');
+
+// Display the specified product.
+Route::get('admin/product/{product}', [ProductController::class, 'adminShow'])->name('admin.product.show');
+
+// Show the form for editing the specified product.
+Route::get('admin/product/{product}/edit', [ProductController::class, 'edit'])->name('admin.product.store');
+
+// Update the specified product in storage.
+Route::put('admin/product/{product}', [ProductController::class, 'update'])->name('admin.product.update');
+
+// Remove the specified product from storage.
+Route::delete('admin/product/{product}', [ProductController::class, 'destroy'])->name('admin.product.destroy');
+
+
+
 
 Route::get('privacy-policy', function () {
     return view('privacy-policy');
